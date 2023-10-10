660d11083f2b6e7da8600f825eb068377edffd4e
Sugih Mohammad Galih <59138555+smgalih19@users.noreply.github.com>
2023-10-10 23:08:00 +0700

    Bissmillah

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+  <div class="container">
+    <a class="navbar-brand" href="/">e-Ticket</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link">Lihat Tiket</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link ">Tentang Kami</a>
+        </li>
+      </ul>
+
+      <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a href="/login" class="nav-link"><i class="bi bi-box-arrow-in-right"></i>
+              Login</a>
+          </li>
+        </ul>
+
+    </div>
+  </div>
+</nav>
