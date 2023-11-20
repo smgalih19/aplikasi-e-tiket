062554b383c363827c07a16469fd03d1a76058fa
Sugih Mohammad Galih <59138555+smgalih19@users.noreply.github.com>
2023-11-21 06:41:43 +0700

    update view tiket dan style

--- resources/views/viewtiket.blade.php
+++ resources/views/viewtiket.blade.php
@@ -7,20 +7,13 @@
                     <h5 class="tiket-pilih">Pilih tiket dibawah ini</h5>
                     <div class="macam-tiket">
 
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-                            <label class="form-check-label" for="flexRadioDefault1">
-                                Pilih
-                            </label>
-                        </div>
-
                         <p>Tiket Sepeda Motor</p>
                         <hr>
                         <h3 class="text-danger">IDR 20.000</h3>
                         <p>Lihat informasi Penukaran Tiket, dan Syarat Ketentuan <span class="text-primary klik-cursor"
                                 data-bs-toggle="modal" data-bs-target="#exampleModal"> di sini.</span></p>
 
-                        <!-- Modal -->
+                        {{-- Modal --}}
                         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
                             aria-hidden="true">
                             <div class="modal-dialog">
@@ -44,12 +37,6 @@
 
                     </div><br>
                     <div class="macam-tiket">
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
-                            <label class="form-check-label" for="flexRadioDefault2">
-                                Pilih
-                            </label>
-                        </div>
                         <p>Tiket Jeep / Sedan dan sejenisnya</p>
                         <hr>
                         <h3 class="text-danger">IDR 60.000</h3>
@@ -58,14 +45,51 @@
                     </div>
                 </div>
 
+                {{-- Awal Bagian Tiket Terpilih --}}
                 <div class="tiket col mt-5 mx-4">
                     <h5 class="tiket-pilih">Tiket yang dipilih</h5>
                     <div class="edit-tiket">
-                        <p>Tiket Kendaraan Motor</p>
+                        <p>Tiket Jeep / Sedan dan sejenisnya</p>
                     </div>
-                    <h3 class="text-danger">18.000</h3>
+                    <h3 class="text-danger">IDR 60.000</h3>
                     <hr>
+
+
+                    <form>
+                        <div class="row mb-3">
+                            <label for="specificSizeInputName" class="col-sm-2 col-form-label">Nama</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" placeholder="Nama Lengkap" aria-label="First name">
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                            <div class="col-sm-10">
+                                <input type="email" class="form-control" id="inputEmail3" placeholder="Alamat Email">
+                            </div>
+                        </div>
+                        <fieldset class="row mb-3">
+                            <legend class="col-form-label col-sm-2 pt-0">Jenis Kelamin</legend>
+                            <div class="col-sm-10">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
+                                    <label class="form-check-label" for="gridRadios1">
+                                    Laki-Laki
+                                    </label>
+                                </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
+                                <label class="form-check-label" for="gridRadios2">
+                                    Perempuan
+                                </label>
+                            </div>
+                            </div>
+                        </fieldset>
+                        <button type="submit" class="btn btn-primary">Lanjutkan</button>
+                    </form>
+
                 </div>
+
 
             </div>
         </div>
